ce99aa0dd98a7d7a335f864b03f10c9261484a4e
kruboy <starkkruboy@gmail.com>
2021-01-18 01:30:36 +0200

    add table generation and fix bg for headers itc

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,56 +1,10 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
+import { createTable } from "./table.template";
 
 export class Table extends ExcelComponent {
   static className = "excel__table";
 
   toHTML() {
-    return `
-    <div class="excel__table__row">
-            <div class="excel__table__info">0</div>
-            <div class="excel__table__data">
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-              <div class="excel__table__column">A</div>
-              <div class="excel__table__column">B</div>
-              <div class="excel__table__column">C</div>
-            </div>
-          </div>
-          <div class="excel__table__row">
-            <div class="excel__table__info">1</div>
-            <div class="excel__table__data">
-              <div class="excel__table__cell selected" contenteditable="">
-                cell
-              </div>
-              <div class="excel__table__cell" contenteditable="">fafa</div>
-              <div class="excel__table__cell" contenteditable="">fafafab</div>
-            </div>
-          </div>
-          <div class="excel__table__row">
-            <div class="excel__table__info">1</div>
-            <div class="excel__table__data">
-              <div class="excel__table__cell">cell</div>
-              <div class="excel__table__cell">fafa</div>
-              <div class="excel__table__cell">fafafab</div>
-            </div>
-          </div>`;
+    return createTable();
   }
 }
