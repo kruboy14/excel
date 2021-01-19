d9f4b95c6aa32e03157e23d1e3cb53d40d3bc02b
kruboy <starkkruboy@gmail.com>
2021-01-19 17:12:00 +0200

    decompose

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,6 @@
 import { $ } from "../../core/dom";
 import { ExcelComponent } from "../../core/ExcelComponent";
-import { isCell, resizeChecker } from "./table.functions";
+import { isCell, matrix, resizeChecker } from "./table.functions";
 import { tableResize } from "./table.resize";
 import { createTable } from "./table.template";
 import { TableSelection } from "./TableSelection";
@@ -35,28 +35,13 @@ export class Table extends ExcelComponent {
     } else if (isCell(event)) {
       const $target = $(event.target);
       if (event.shiftKey) {
-        const target = $target.id(true);
-        const current = this.selection.current.id(true);
-        const cols = range(current.col, target.col);
-        const rows = range(current.row, target.row);
-
-        const ids = cols.reduce((acc, col) => {
-          rows.forEach((row) => acc.push(`${row}:${col}`));
-          return acc;
-        }, []);
-        const cells = ids.map((id) => this.$root.find(`[data-id="${id}"]`));
-        this.selection.selectGroup(cells)
-        console.log(cells);
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`)
+        );
+        this.selection.selectGroup($cells);
       } else {
         this.selection.select($target);
       }
     }
   }
-}
-
-function range(start, end) {
-  if (start > end) {
-    [end, start] = [start, end];
-  }
-  return new Array(end - start + 1).fill("").map((_, i) => start + i);
 }
--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -3,5 +3,25 @@ export function resizeChecker(event) {
 }
 
 export function isCell(event) {
-  return event.target.dataset.type === "cell" 
+  return event.target.dataset.type === "cell";
+}
+
+export function range(start, end) {
+  if (start > end) {
+    [end, start] = [start, end];
+  }
+  return new Array(end - start + 1).fill("").map((_, i) => start + i);
+}
+
+export function matrix($target, $current) {
+  const target = $target.id(true);
+  const current = $current.id(true);
+
+  const cols = range(current.col, target.col);
+  const rows = range(current.row, target.row);
+
+  return cols.reduce((acc, col) => {
+    rows.forEach((row) => acc.push(`${row}:${col}`));
+    return acc;
+  }, []); 
 }
